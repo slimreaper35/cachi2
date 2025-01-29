9c1ec0e24e79f66147fa1caa1dceaf264d7280a0
Michal Šoltis <msoltis@redhat.com>
2025-01-29 15:56:42 +0100

    utils: Replace "universal_newlines" with "text" param for subprocess.run
    In Python3.7, the new "text" parameter was added as a more
    understandable alias of "universal_newlines". The "universal_newlines"
    is provided for backwards compatibility [1].
    
    Altough, I haven't found an information about deprecation, let's follow
    best practises and use the newer parameter for subprocess configuration.
    
    ---
    [1]: https://docs.python.org/3/library/subprocess.html#older-high-level-api
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- cachi2/core/utils.py
+++ cachi2/core/utils.py
@@ -32,7 +32,7 @@ def run_cmd(cmd: Sequence[str], params: dict) -> str:
     :raises CalledProcessError: if the command fails
     """
     params.setdefault("capture_output", True)
-    params.setdefault("universal_newlines", True)
+    params.setdefault("text", True)
     params.setdefault("encoding", "utf-8")
 
     conf = get_config()
